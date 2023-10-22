04d94e792492ac6936304d5c2ab66cf8d8bcc765
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2023-10-22 16:53:31 +0200

    App.js: ADDED TouchableComponent WITHOUT FEEDBACK

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableWithoutFeedback } from 'react-native';
 
+function handlePress() {
+  console.log("TOUCHED");
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView style={styles.container}>
+    
+      <Text style={styles.header1}>Touchable Components Examples</Text>
+
+      <View style={styles.tilesContainer}>
+
+        {/* TouchableWithoutFeedback */}
+        <TouchableWithoutFeedback onPress={() => handlePress()}>
+          <View style={styles.tile}>
+            <Text style={styles.tileHeader}>Without Feedback</Text>
+            <Text style={styles.tileText}>press me</Text>
+          </View>
+        </TouchableWithoutFeedback>  
+
       <StatusBar style="auto" />
-    </View>
+      </View>
+    </ScrollView>
   );
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   header1: {
+    fontSize: 24,
+    fontWeight: '600',
+    margin: 10,
   },
+  container: {
+    backgroundColor: '#EDCCD4',
+    paddingTop: 56,
+  },
+  tilesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexGrow: 0,
+    flexShrink: 0,
+    justifyContent: 'space-evenly',
+
+  },
+  tile: {
+    flexBasis: '50%',//200
+    borderRadius: 5,
+    borderColor: '#B4B8DA',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    marginVertical: '2%',
+    padding: 10,
+  },
+  tileHeader: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '600',
+  },
+  tileText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  }
 });
